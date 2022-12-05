e90c709540fa1b197175753aade3d936f4673cc6
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-12-05 09:06:20 -0800

    Implemented mouseover and mouseout function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // The svg
 
 const width = 900;
-const height = 600; 
-    
+const height = 600;
+
 const svg = d3.select("svg").attr("width", width).attr("height", height);
 
 // Map and projection. Try:  d3.geoAiry() / d3.geoAitoff() / d3.geoArmadillo() / d3.geoAugust() / d3.geoAzimuthalEqualArea() / d3.geoAzimuthalEquidistant() and more
@@ -14,98 +14,138 @@ const projection = d3.geoMercator()
 let map_data = d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
 
 
-    // Draw the map
-    map_data.then(function(data) {
+// Draw the map
+map_data.then(function (data) {
     const countries = svg.append("g")
         .selectAll("path")
         .data(data.features)
         .join("path")
-            .attr('class', 'map')
-            .attr("d", d3.geoPath()
-                .projection(projection)
-            )
-            .append('country')
-            .text(d => (d.properties.name))
+        .attr('class', 'map')
+        .attr("d", d3.geoPath()
+            .projection(projection)
+        )
+        .append('country')
+        .text(d => (d.properties.name))
+});
+
+
+
+
+
+
+let world_map = document.querySelector("#my_dataviz")
+// world_map.addEventListener("click",  async (e) => {
+
+
+//     e.preventDefault();
+//     const titleHtml = e.path[0];
+//     let dataDiv = document.querySelector("#data"); 
+//     let deathsDiv = document.querySelector("#deaths"); 
+//     let countrynameDiv = document.querySelector(".country-name"); 
+//     // console.log(e)
+//     // console.log(e.path[1])
+//     if (e.target.tagName === 'path') {
+
+//         const a = titleHtml.querySelector("country");
+//         console.log(a)
+//         const name_of_country = a.innerHTML;
+
+//         const data1 = await (get_data(name_of_country));
+//         // console.log(data1)
+
+
+//         // console.log(data1)
+//         let sum_of_active_cases = 0;
+//         let no_of_deaths = 0;
+//         // let country_name;
+//         for (let i = 0; i < data1.length; i++) {
+
+//             // country_name = data1[i].Country;
+//             sum_of_active_cases += data1[i].Active; 
+//             no_of_deaths += data1[i].Deaths;
+
+//         }
+
+//             countrynameDiv.innerHTML = `Country: ${name_of_country}`
+//             dataDiv.innerHTML = `Active cases: ${sum_of_active_cases}`;
+//             deathsDiv.innerHTML = `Deaths cases: ${no_of_deaths}`;
+//     } else {
+//         dataDiv.innerHTML = '';
+//         deathsDiv.innerHTML = '';
+//         countrynameDiv.innerHTML = '';
+
+//     }
+// });   
+
+const tooltipDiv = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+world_map.addEventListener("mouseover", async (e) => {
+    e.preventDefault();
+    const[x, y] = d3.pointer(e);
+    const titleHtml = e.path[0];
+    let countrynameDiv = document.querySelector(".country-name");
+    if (e.target.tagName === 'path') {
+
+        const a = titleHtml.querySelector("country");
+        // console.log(a)
+        const name_of_country = a.innerHTML;
+
+        const data1 = await (get_data(name_of_country));
+        // console.log(data1)
+        // console.log(data1)
+        let sum_of_active_cases = 0;
+        let no_of_deaths = 0;
+        // let country_name;
+        for (let i = 0; i < data1.length; i++) {
+            // country_name = data1[i].Country;
+            sum_of_active_cases += data1[i].Active;
+            no_of_deaths += data1[i].Deaths;
+        }
+        const tooltipData = `${name_of_country}  <p>Active cases: ${sum_of_active_cases}</p>`
+        tooltipDiv.transition().duration(200).style("opacity", .9);
+        tooltipDiv.html(tooltipData)  
+            .style("left", (x + 10) + "px")     
+            .style("top", (y - 10) + "px");    
+    } 
+
     });
 
-    // const tooltipDiv = d3.select("svg").append("div")    
-    // .attr("class", "tooltip")               
-    // .style("opacity", 0);
+    world_map.addEventListener("mouseout", async (e) => {
+        tooltipDiv.transition().duration(500).style("opacity", 0);
+    })
 
 
-    
-    
-    let map = document.querySelector("#my_dataviz")
-    map.addEventListener("click",  async (e) => {
-        e.preventDefault();
-        const titleHtml = e.path[0];
-        let dataDiv = document.querySelector("#data"); 
-        let deathsDiv = document.querySelector("#deaths"); 
-        let countrynameDiv = document.querySelector(".country-name"); 
-        // console.log(e)
-        // console.log(e.path[1])
-        if (e.target.tagName === 'path') {
-
-            const a = titleHtml.querySelector("country");
-            console.log(a)
-            const name_of_country = a.innerHTML;
-
-            const data1 = await (get_data(name_of_country));
-            // console.log(data1)
-
-            
-            // console.log(data1)
-            let sum_of_active_cases = 0;
-            let no_of_deaths = 0;
-            // let country_name;
-            for (let i = 0; i < data1.length; i++) {
-                
-                // country_name = data1[i].Country;
-                sum_of_active_cases += data1[i].Active; 
-                no_of_deaths += data1[i].Deaths;
-
-            }
-            
-                countrynameDiv.innerHTML = `Country: ${name_of_country}`
-                dataDiv.innerHTML = `Active cases: ${sum_of_active_cases}`;
-                deathsDiv.innerHTML = `Deaths cases: ${no_of_deaths}`;
-        } else {
-            dataDiv.innerHTML = '';
-            deathsDiv.innerHTML = '';
-            countrynameDiv.innerHTML = '';
-
+const get_data = async (ele) => {
+    if (typeof ele === 'undefined') {
+        return '';
+    } else {
+        try {
+            let res = await fetch(`https://api.covid19api.com/live/country/${ele}/status/confirmed/date/2022-09-10T13:13:30Z`);
+            return await res.json();
+        } catch (err) {
+            console.error(err);
         }
-    });   
-    
-    
-    
-    const get_data = async (ele) => {
-        if (typeof ele === 'undefined') {
-            return '';
-        } else {
-                try{
-                    let res = await fetch(`https://api.covid19api.com/live/country/${ele}/status/confirmed/date/2022-09-10T13:13:30Z`);
-                    return await res.json();
-                } catch(err) {
-                    console.error(err);
-                }
-            }
     }
+}
 
 
 
 
 
-    
-
-    
 
 
 
-    
-    
 
-    
+
+
+
+
+
+
+
+
 
 
 
